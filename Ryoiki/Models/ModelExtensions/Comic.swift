859285fcfc21c64092df7b02dc2a9bef9e8bb2ec
Amelia Frost <afrosty.skye@gmail.com>
2025-10-26 00:15:24 -0700

    coverImage should be a thumbnail

--- Ryoiki/Models/ModelExtensions/Comic.swift
+++ Ryoiki/Models/ModelExtensions/Comic.swift
@@ -6,13 +6,51 @@
 //
 
 import Foundation
+import ImageIO
+import UniformTypeIdentifiers
 
 extension Comic {
-    /// Sets the cover image from a file URL by loading its data.
-    /// If loading fails, the cover image is not modified.
-    func setCoverImage(from fileURL: URL) {
-        if let data = try? Data(contentsOf: fileURL) {
-            self.coverImage = data
+    @MainActor
+    func setCoverImage(from fileURL: URL, maxPixel: CGFloat = 512, compressionQuality: CGFloat = 0.8) {
+
+        Task { @MainActor in
+            // Synchronous worker that performs ImageIO work
+            func makeThumbnailDataSync() -> Data? {
+                let options: [CFString: Any] = [
+                    kCGImageSourceCreateThumbnailWithTransform: true,
+                    kCGImageSourceCreateThumbnailFromImageAlways: true,
+                    kCGImageSourceThumbnailMaxPixelSize: Int(maxPixel)
+                ]
+
+                guard let src = CGImageSourceCreateWithURL(fileURL as CFURL, nil),
+                      let cgThumb = CGImageSourceCreateThumbnailAtIndex(src, 0, options as CFDictionary) else {
+                    return nil
+                }
+
+                let data = NSMutableData()
+                let jpegUTI = UTType.jpeg.identifier as CFString
+                guard let dest = CGImageDestinationCreateWithData(data as CFMutableData, jpegUTI, 1, nil) else {
+                    return nil
+                }
+                let props: [CFString: Any] = [
+                    kCGImageDestinationLossyCompressionQuality: min(max(compressionQuality, 0), 1)
+                ]
+                CGImageDestinationAddImage(dest, cgThumb, props as CFDictionary)
+                guard CGImageDestinationFinalize(dest) else { return nil }
+                return data as Data
+            }
+
+            // Offload to background queue to avoid blocking the main actor
+            let resultData: Data? = await withCheckedContinuation { continuation in
+                DispatchQueue.global(qos: .userInitiated).async {
+                    let data = makeThumbnailDataSync() ?? (try? Data(contentsOf: fileURL))
+                    continuation.resume(returning: data)
+                }
+            }
+
+            if let resultData {
+                self.coverImage = resultData
+            }
         }
     }
 
